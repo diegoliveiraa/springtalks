43e266adf5de8cc6174f6fd83ade26fab739e330
Diego Oliveira <diegoliveiraa1@gmail.com>
2025-04-10 15:33:28 -0300

    feta: create configs file

---
+++ springtalks/src/main/java/com/diego/springtalks/configs/WebSocketConfig.java
@@ -0,0 +1,24 @@
+package com.diego.springtalks.configs;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws-chat").setAllowedOriginPatterns("*"); // WebSocket puro
+        registry.addEndpoint("/ws-chat-sockjs").setAllowedOriginPatterns("*").withSockJS(); // SockJS (ex: front-end com Stomp.js)
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/topic");
+        registry.setApplicationDestinationPrefixes("/app");
+    }
+}
