7477de282e8bfaa703469fd6f362ccfcc8033483
Diego Oliveira <diegoliveiraa1@gmail.com>
2025-04-10 15:36:13 -0300

    feat: initial config

---
+++ springtalks/src/main/java/com/diego/springtalks/SpringtalksApplication.java
@@ -0,0 +1,13 @@
+package com.diego.springtalks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringtalksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringtalksApplication.class, args);
+    }
+
+}
