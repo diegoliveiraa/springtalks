7a073a1f98b8fed221dedf85150ba63bcb797374
Diego Oliveira <diegoliveiraa1@gmail.com>
2025-04-10 15:32:00 -0300

    feta: initial config

---
+++ springtalks/src/test/java/com/diego/springtalks/SpringtalksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diego.springtalks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringtalksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
